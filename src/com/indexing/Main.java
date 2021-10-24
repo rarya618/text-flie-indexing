d2cff0912afb4349cacd9d568310151dd3ef6711
Russal Arya <russalarya@gmail.com>
2021-10-24 09:53:00 +0300

    Added Project Files

---
+++ src/com/indexing/Main.java
@@ -0,0 +1,17 @@
+package com.indexing;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        // Write a Java application that provides a service for indexing text files.
+        // Console interface should allow for
+        // a. specifying the indexed files and directories and
+        // b. querying files containing a given word.
+        // Library should be extensible by the tokenization algorithm (simple splitting by words/support lexers/etc.).
+        // State persistence between running sessions is not needed.
+        // Providing some tests and a program with usage examples is advised.
+        
+
+    }
+}
