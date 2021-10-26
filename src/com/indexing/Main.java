7caae7f35a60a9309a0b4d224f121512481a2b23
Russal Arya <russalarya@gmail.com>
2021-10-26 12:11:09 +0300

    Basic command processing

--- src/com/indexing/Main.java
+++ src/com/indexing/Main.java
@@ -1,5 +1,11 @@
 package com.indexing;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Main {
 
     private String readFromInputStream(InputStream inputStream) throws IOException {
@@ -24,7 +30,50 @@ public class Main {
         // Library should be extensible by the tokenization algorithm (simple splitting by words/support lexers/etc.).
         // State persistence between running sessions is not needed.
         // Providing some tests and a program with usage examples is advised.
-        
+
+        // Setting up the input scanner
+        Scanner sc = new Scanner(System.in);
+
+        // Introducing the user to the application
+        System.out.println("Welcome to text-file-indexing!");
+        System.out.println("Use 'help' for the list of commands available.");
+        System.out.println("Happy indexing!");
+
+        boolean running = true;
+
+        while(running) {
+            // command input
+            System.out.print(">> ");
+            String command = sc.nextLine();
+
+            // command preprocessing
+            command = command.strip();
+
+            // 'quit' commands
+            if (command.equals("quit")) {
+                running = false;
+            }
+
+            // 'help' command
+            else if (command.equals("help")) {
+                System.out.println("Commands:");
+                System.out.println("add: adds files for indexing");
+                System.out.println("quit: quits application");
+            }
+
+            // 'add' command
+            else if (command.equals("add")) {
+                System.out.print("Enter file name: ");
+                String fileName = sc.nextLine();
+            }
+
+            // error check
+            else {
+                System.out.println(command);
+                System.out.println("Invalid command");
+            }
+        }
+
 
     }
 }
